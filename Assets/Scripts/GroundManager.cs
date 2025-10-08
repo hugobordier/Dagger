144e1c51ea88cb89b0a20d541bab33b7c9aa377a
FantinLNA <fantin.ellena--mehl@epfedu.fr>
2025-10-08 11:22:22 +0200

    first build

---
+++ Assets/Scripts/GroundManager.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundManager : MonoBehaviour
+{
+
+    [SerializeField] private Transform player;
+    [SerializeField] private GameObject groundPrefab;
+    public float groundWidth = 20f; // largeur du prefab
+    private List<GameObject> grounds = new List<GameObject>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = -1; i <= 1; i++)
+        {
+            GameObject g = Instantiate(groundPrefab, new Vector3(i * groundWidth, -1.74f, 0), Quaternion.identity);
+            grounds.Add(g);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float playerX = player.position.x;
+        GameObject leftMost = grounds[0];
+        GameObject rightMost = grounds[grounds.Count - 1];
+
+        if (playerX > rightMost.transform.position.x - groundWidth / 2)
+        {
+            // Spawn nouveau à droite
+            GameObject g = Instantiate(groundPrefab, rightMost.transform.position + Vector3.right * groundWidth, Quaternion.identity);
+            grounds.Add(g);
+
+            // Supprime celui de gauche
+            Destroy(leftMost);
+            grounds.RemoveAt(0);
+        }
+        
+        if (playerX < leftMost.transform.position.x + groundWidth/2)
+        {
+            GameObject g = Instantiate(groundPrefab, leftMost.transform.position - Vector3.right * groundWidth, Quaternion.identity);
+            grounds.Insert(0, g);
+
+            Destroy(rightMost);
+            grounds.RemoveAt(grounds.Count - 1);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float m_TranslationSpeed;
+    [SerializeField]
+    private float m_JumpImpulsionMagnitude;
+
+    [SerializeField]
+    private Transform m_camera;
+
+    Rigidbody m_Rb;
+
+    //private bool m_IsGrounded;
+
+    private int m_GroundContacts = 0;
+
+    void Awake()
+    {
+        m_Rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float hInput = 1.0f;
+        bool jump = Input.GetAxis("Jump") > 0 /*|| Input.GetKeyDown(KeyCode.Space)*/;
+
+        Vector3 moveVect = hInput * transform.forward * m_TranslationSpeed * Time.deltaTime;
+        m_Rb.MovePosition(m_Rb.position + moveVect);
+
+        if (jump && m_GroundContacts > 0)
+        {
+            Vector3 jumpForce = Vector3.up * m_JumpImpulsionMagnitude;
+            m_Rb.AddForce(jumpForce, ForceMode.Impulse);
+        }
+
+        if (jump)
+        {
+            Debug.Log("saut");
+        }
+
+        if (m_GroundContacts > 0)
+        {
+            m_Rb.linearVelocity = Vector3.zero;
+            Debug.Log("aux sol");
+        }
+
+        m_Rb.angularVelocity = Vector3.zero;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("touché");
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            //Debug.LogError(Time.frameCount+" colLocalPt = " + colLocalPt+ "   colLocalPt.magnitude = "+ colLocalPt.magnitude);
+
+            m_GroundContacts++;
+            Debug.Log("Au sol (" + m_GroundContacts + ")");
+        }
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("quitter");
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            m_GroundContacts = Mathf.Max(0, m_GroundContacts - 1);
+            Debug.Log("Quitter sol (" + m_GroundContacts + ")");
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (m_camera != null)
+        {
+            Vector3 camPos = m_camera.position;
+            camPos.x = m_Rb.position.x;
+            m_camera.position = camPos;
+        }
+    }
+}
